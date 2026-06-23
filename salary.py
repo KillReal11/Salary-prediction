54ec7c44f7ae2f6cb8838bbfb57211867eef8094
Kirill <dawiduk2013@yandex.ru>
2026-06-23 22:51:06 +0500

    Update salary.py

--- salary.py
+++ salary.py
@@ -7,36 +7,6 @@ from terminaltables import SingleTable
 from dotenv import load_dotenv
 
 
-def count_vacancies_total_habr(vacancy_name):
-    url = 'https://career.habr.com/api/frontend/vacancies'
-    params = {
-        'q': vacancy_name,
-        'locations[]': 'c_678'
-        }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    vacancies = response.json()
-    vacancies_found = vacancies.get('meta').get('totalResults')
-    return vacancies_found
-
-
-def count_vacancies_total_sj(vacancy_name, token):
-    url = 'https://api.superjob.ru/2.0/vacancies/'
-    params = {
-        'keyword': vacancy_name,
-        'town': 'Москва',
-        'catalogues': 48,
-        }
-    headers = {
-        'X-Api-App-Id': token,
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(url, params=params, headers=headers)
-    response.raise_for_status()
-    vacancies_all_count = response.json().get('total')
-    return vacancies_all_count
-
-
 def predict_salary(salary_from, salary_to):
     if not salary_from and not salary_to:
         return None
@@ -51,113 +21,142 @@ def predict_salary(salary_from, salary_to):
     return mean_salary
 
 
-def predict_rub_salary_habr(vacancy_name):
-    salary_list = []
+def get_vacancies_habr(vacancy_name):
+    vacancies_all = []
     for page in count(0):
         url = 'https://career.habr.com/api/frontend/vacancies'
         params = {
             'q': vacancy_name,
             'page': page,
             'locations[]': 'c_678'
-            }
+        }
         response = requests.get(url, params=params)
         response.raise_for_status()
         vacancies = response.json()
         total_pages = vacancies.get('meta').get('totalPages')
         if page >= total_pages:
             break
-        vacancies_list = vacancies.get('list')
-        for vacancy in vacancies_list:
-            salary = vacancy.get('salary')
-            salary_currency = salary.get('currency')
-            salary_from = salary.get('from')
-            salary_to = salary.get('to')
-            if salary_currency == 'rur':
-                mean_salary = predict_salary(salary_from, salary_to)
-                salary_list.append(mean_salary)
-    salary_list = [salary for salary in salary_list if salary is not None]
-    return salary_list
+        vacancies_on_page = vacancies.get('list')
+        vacancies_all.append(vacancies_on_page)
+    vacancies_found = vacancies.get('meta').get('totalResults')
+    return vacancies_all, vacancies_found
 
 
-def predict_rub_salary_sj(vacancy_name, token):
+def get_vacancies_sj(vacancy_name, token):
     vacancies_per_page = 100
-    salary_list = []
+    city_code_moskow = 48
+    vacancies_all = []
     for page in count(0):
         url = 'https://api.superjob.ru/2.0/vacancies/'
         params = {
             'keyword': vacancy_name,
             'town': 'Москва',
-            'catalogues': 48,
+            'catalogues': city_code_moskow,
             'page': page,
             'count': vacancies_per_page
-            }
+        }
         headers = {
             'X-Api-App-Id': token,
             'Content-Type': 'application/json'
         }
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
-        vacancies_all = response.json().get('objects')
-        vacancies_all_count = response.json().get('total')
-        total_pages = math.ceil(vacancies_all_count / vacancies_per_page)
+        vacancies_found = response.json().get('total')
+        total_pages = math.ceil(vacancies_found / vacancies_per_page)
         if page >= total_pages:
             break
-        for vacancy in vacancies_all:
+        vacancies_on_page = response.json().get('objects')
+        vacancies_all.append(vacancies_on_page)
+        time.sleep(2)
+    vacancies_found = response.json().get('total')
+    return vacancies_all, vacancies_found
+
+
+def predict_rub_salary_habr(vacancy_name):
+    vacancies_all, __ = get_vacancies_habr(vacancy_name)
+    salary_all = []
+    for page in vacancies_all:
+        for vacancy in page:
+            salary = vacancy.get('salary')
+            salary_currency = salary.get('currency')
+            salary_from = salary.get('from')
+            salary_to = salary.get('to')
+            if salary_currency == 'rur':
+                mean_salary = predict_salary(salary_from, salary_to)
+                salary_all.append(mean_salary)
+    salary_all = [salary for salary in salary_all if salary is not None]
+    return salary_all
+
+
+def predict_rub_salary_sj(vacancy_name, token):
+    salary_all = []
+    vacancies_all, __ = get_vacancies_sj(vacancy_name, token)
+    for page in vacancies_all:
+        for vacancy in page:
             salary_currency = vacancy.get('currency')
             salary_from = vacancy.get('payment_from')
             salary_to = vacancy.get('payment_to')
             if salary_currency == 'rub':
                 mean_salary = predict_salary(salary_from, salary_to)
-                salary_list.append(mean_salary)
-        time.sleep(2)
-    salary_list = [salary for salary in salary_list if salary is not None]
-    return salary_list
+                salary_all.append(mean_salary)
+    salary_all = [salary for salary in salary_all if salary is not None]
+    return salary_all
 
 
-def get_vacancy_stats_habr(keywords):
-    vacancy = dict.fromkeys(keywords)
-    for key in keywords:
+def get_vacancy_stats_habr(vacancy_name):
+    vacancy = dict.fromkeys(vacancy_name)
+    for name in vacancy_name:
         vacancy_stats = {}
-        salary_list = predict_rub_salary_habr(key)
-        if salary_list:
-            vacancy_stats['vacancies_found'] = count_vacancies_total_habr(key)
-            vacancy_stats['vacancies_processed'] = len(salary_list)
-            vacancy_stats['average_salary'] = int(sum(salary_list) / len(salary_list))
+        salary_all = predict_rub_salary_habr(name)
+        __, vacancies_found = get_vacancies_habr(name)
+        if salary_all:
+            vacancy_stats = {
+                'vacancies_found': vacancies_found,
+                'vacancies_processed': len(salary_all),
+                'average_salary': int(sum(salary_all) / len(salary_all))
+            }
         else:
-            vacancy_stats['vacancies_found'] = count_vacancies_total_habr(key)
-            vacancy_stats['vacancies_processed'] = 0
-            vacancy_stats['average_salary'] = 0
-        vacancy[key] = vacancy_stats
+            vacancy_stats = {
+                'vacancies_found': vacancies_found,
+                'vacancies_processed': 0,
+                'average_salary': 0
+            }
+        vacancy[name] = vacancy_stats
     return vacancy
 
 
-def get_vacancy_stats_sj(keywords, token):
-    vacancy = dict.fromkeys(keywords)
-    for key in keywords:
+def get_vacancy_stats_sj(vacancy_name, token):
+    vacancy = dict.fromkeys(vacancy_name)
+    for name in vacancy_name:
         vacancy_stats = {}
-        salary_list = predict_rub_salary_sj(key, token)
-        if salary_list:
-            vacancy_stats['vacancies_found'] = count_vacancies_total_sj(key, token)
-            vacancy_stats['vacancies_processed'] = len(salary_list)
-            vacancy_stats['average_salary'] = int(sum(salary_list) / len(salary_list))
+        salary_all = predict_rub_salary_sj(name, token)
+        __, vacancies_found = get_vacancies_sj(name, token)
+        if salary_all:
+            vacancy_stats = {
+                'vacancies_found': vacancies_found,
+                'vacancies_processed': len(salary_all),
+                'average_salary': int(sum(salary_all) / len(salary_all))
+            }
         else:
-            vacancy_stats['vacancies_found'] = count_vacancies_total_sj(key, token)
-            vacancy_stats['vacancies_processed'] = 0
-            vacancy_stats['average_salary'] = 0
-        vacancy[key] = vacancy_stats
+            vacancy_stats = {
+                'vacancies_found': vacancies_found,
+                'vacancies_processed': 0,
+                'average_salary': 0
+            }
+        vacancy[name] = vacancy_stats
     return vacancy
 
 
 def display_table_output(table_title, vacancy_stats: dict):
     subtitle = ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
-    stats_list = [subtitle]
+    stats_all = [subtitle]
     for name, stats in vacancy_stats.items():
         vacancy_stats = []
         vacancy_stats.append(name)
         for stat_values in stats.values():
             vacancy_stats.append(stat_values)
-        stats_list.append(vacancy_stats)
-    table_instance = SingleTable(stats_list, table_title)
+        stats_all.append(vacancy_stats)
+    table_instance = SingleTable(stats_all, table_title)
     table_instance.justify_columns[2] = 'right'
     print(table_instance.table)
     print()
